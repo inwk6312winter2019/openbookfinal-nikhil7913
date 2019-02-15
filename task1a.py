a108fe8837a577334d5349dd4f8cbd5b4bc0b894
nikhil <Nikhil.Nikhi@dal.ca>
2019-02-15 10:10:23 -0800

    first

---
+++ task1a.py
@@ -0,0 +1,28 @@
+filename="Book1.txt"
+file=open(filename,"r")
+
+def unique_words(book):
+   for l in file:
+    tu={} 
+    l=l.strip()
+      for w in line:    
+         
+
+
+def count_the_article:
+  list=["a", "the", "at", "run", "to","and","are","or","for","an","this"]
+
+
+def sorted_words(book1):
+   for line2 in book1:
+      fields2 = line2.rstrip('\n').split('\t')
+      fields2.sort()
+      print fields2
+
+
+def character_word_count(book2):
+   tup1={}
+   for line3 in book2:
+      string=string.split()
+   for word in string:
+       
