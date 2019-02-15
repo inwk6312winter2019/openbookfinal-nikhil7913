18e2905bd12c5f7c1367b474922816c80c4a13eb
nikhil <Nikhil.Nikhi@dal.ca>
2019-02-15 10:29:51 -0800

    second

--- task1a.py
+++ task1a.py
@@ -2,16 +2,23 @@ filename="Book1.txt"
 file=open(filename,"r")
 
 def unique_words(book):
-   for l in file:
-    tu={} 
-    l=l.strip()
-      for w in line:    
+   for line in book:
+    line=line.strip()
+      for word in line:    
          
 
 
-def count_the_article:
-  list=["a", "the", "at", "run", "to","and","are","or","for","an","this"]
-
+def count_the_article(book3):
+   count=0   
+   words=["a", "the", "at", "run", "to","and","are","or","for","an","this"]  
+   for line1 in book3:
+      line=line.strip()
+   for word in line1:
+      for i in range(len(words)) 
+        if word=words[i]:
+          count=count+1
+   print(count)
+ 
 
 def sorted_words(book1):
    for line2 in book1:
@@ -25,4 +32,4 @@ def character_word_count(book2):
    for line3 in book2:
       string=string.split()
    for word in string:
-       
+      print(tup1[word]=len(word)) 
